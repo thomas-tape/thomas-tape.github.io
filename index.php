fcc74fa513725dc7903659c9ffdc4b417b1d0c15
Thomas Tapella <60255711+thomas-tape@users.noreply.github.com>
2020-09-01 12:24:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php ?>
+<!doctype html>
+<html lan="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Thomas Tapella's personal website</title>
+    <link rel="stylesheet" href="src/style.css" type="text/css">
+    <link rel="stylesheet" href="src/fonts/fonts.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<div class="header">
+    <video autoplay loop class="video-background" muted plays-inline>
+        <source src="src/video/bg1.mp4" type="video/mp4">
+    </video>
+    <div class="welcome">
+    <h1>Thomas Tapella</h1>
+        <div class="social-icons">
+            <a href="#">
+                <img src="src/icons/instagram.svg"></img>
+            </a>
+            <a href="#">
+                <img src="src/icons/facebook.svg"></img>
+            </a>
+            <a href="#">
+                <img src="src/icons/linkedin.svg"></img>
+            </a>
+            <a href="#">
+                <img src="src/icons/mail.svg"></img>
+            </a>
+            <a href="#">
+                <img src="src/icons/message-square.svg"></img>
+            </a>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
